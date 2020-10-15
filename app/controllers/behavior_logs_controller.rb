aad1a4885954496b597d80b27dd836d758498e93
tpham13 <tpham0123@gmail.com>
2020-10-15 14:05:33 -0700

    add user and kid show pages

--- app/controllers/behavior_logs_controller.rb
+++ app/controllers/behavior_logs_controller.rb
@@ -1,2 +1,30 @@
 class BehaviorLogsController < ApplicationController
+    def index
+    end 
+
+    def show
+    end 
+
+    def new
+        @behavior_log = BehaviorLog.new
+    end 
+
+    def create
+        @behavior_log = current_user.kids.behavior_logs.new(behavior_log_params)
+        byebug
+        if @behavior_log.save
+            
+            redirect_to behavior_logs_path
+        else
+            render :new 
+        end 
+    end 
+
+    private
+
+        def behavior_log_params
+            params.require(:behavior_log).permit(:date, :time, :location, :before_behavior, :behavior_content, :outcome)
+        end 
+
+
 end
--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -1,2 +1,31 @@
 class KidsController < ApplicationController
+
+    def index
+        @kids = current_user.kids.all
+    end 
+
+    def show
+        @kid = Kid.find(params[:id])
+    end 
+    
+    def new
+        @kid = Kid.new
+    end 
+
+    def create
+        # byebug
+        @kid = Kid.new(kid_params)
+        if @kid.save
+            redirect_to @kids
+        else
+            render :new
+        end 
+        
+    end 
+
+    private
+
+        def kid_params
+            params.require(:kid).permit(:name, :birthday)
+        end 
 end
