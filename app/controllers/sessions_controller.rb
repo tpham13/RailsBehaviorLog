a55dd8eec58737f6c4dd0ce649f266cf720d344f
tpham13 <tpham0123@gmail.com>
2020-10-28 10:57:25 -0700

    separate #create and #facebook into two dif methods in session control

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,36 +6,50 @@ class SessionsController < ApplicationController
     end 
 
     def create 
-        if auth
-            @user = User.find_or_create_by(email: auth["info"]["email"]) do |u| #if user is exist then find the user by email by the returned hash 
-                #then we're going to set the user attributes: username and password
-                u.username = auth["info"]["name"]
-                u.password = SecureRandom.hex(10)
-        end 
-        #if the user is saved, then we'll set the session[:user_id] = @user.id
-        if @user.persisted?
-            session[:user_id] = @user.id
-            redirect_to @user
-        else
-            #if user isn't saved then redirect_to root_path
-            redirect_to '/'
-        end 
-        else
+        # if auth
+        #     @user = User.find_or_create_by(email: auth["info"]["email"]) do |u| #if user is exist then find the user by email by the returned hash 
+        #         #then we're going to set the user attributes: username and password
+        #         u.username = auth["info"]["name"]
+        #         u.password = SecureRandom.hex(10)
+        # end 
+        # #if the user is saved, then we'll set the session[:user_id] = @user.id
+        # if @user.persisted?
+        #     session[:user_id] = @user.id
+        #     redirect_to @user
+        # else
+        #     #if user isn't saved then redirect_to root_path
+        #     redirect_to '/'
+        # end 
+        # else
             
         #     #NORMAL LOGIN PROCESS W/ Bcrypt
-            user = User.find_by(username: params[:user][:username])
-            if user && user.authenticate(params[:user][:password])
-                # set the session so when they login, their data is save in a session while they're login
-                session[:user_id] = user.id 
-                redirect_to user_path(user) 
-            else 
-                flash[:message] = "Incorrect login info, please try again"
-                render :new
-            end
-        end 
-        
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            # set the session so when they login, their data is save in a session while they're login
+            session[:user_id] = user.id 
+            redirect_to user_path(user) 
+        else 
+            flash[:message] = "Incorrect login info, please try again"
+            render :new
+        end     
     end 
 
+   def facebook
+    @user = User.find_or_create_by(email: auth["info"]["email"]) do |u| #if user is exist then find the user by email by the returned hash 
+    #then set the user attibutes
+    u.username = auth["info"]["name"]
+    u.password = SecureRandom.hex(10)
+    end
+    #if the user is saved, then we'll set the session[:user_id] = @user.id
+
+    if @user.persisted?
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+    else
+        #if user isn't saved then redirect_to root_path
+        redirect_to '/'
+    end
+   end 
    private
 
    def auth #this method response with a hash that we get from facebook
--- config/routes.rb
+++ config/routes.rb
@@ -8,18 +8,16 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   #omniauth callback route
-  get '/auth/facebook/callback' => 'sessions#create'
+  get '/auth/facebook/callback' => 'sessions#facebook'
   #logout route
   delete '/logout' => 'sessions#destroy'
   
-  get '/behavior_logs/:id/edit' => 'behavior_logs#edit'
-  post '/behavior_logs/:id/edit' => 'behavior_logs#update'
   resources :behavior_logs
   resources :kids do 
-    resources :behavior_logs, shallow: true
+    resources :behavior_logs #, shallow: true
   end
   resources :users do 
-    resources :behavior_logs, shallow: true
+    resources :behavior_logs #, shallow: true
   end 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
